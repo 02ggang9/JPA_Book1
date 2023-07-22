d2892805b6e0e51c5116c99dd3461863beb571d6
02ggang9 <02ggang9@gmail.com>
2023-07-22 23:54:05 +0900

    Chapter-5: 상품 도메인 개발

--- src/main/java/jpabook/jpabook/domain/item/Item.java
+++ src/main/java/jpabook/jpabook/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpabook.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpabook.domain.Category;
+import jpabook.jpabook.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -27,5 +28,20 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
+
+    /*
+    * stock 증기
+    * */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 
 }
