774f59412841b07f82e7e4bd7409a2b9a36ecaac
02ggang9 <02ggang9@gmail.com>
2023-07-26 23:21:59 +0900

    JPA-2 Chapter-3: V1: 엔티티를 직접 노출

---
+++ src/main/java/jpabook/jpabook/api/OrderSimpleApiController.java
@@ -0,0 +1,24 @@
+package jpabook.jpabook.api;
+
+import jpabook.jpabook.domain.Order;
+import jpabook.jpabook.domain.repository.OrderRepository;
+import jpabook.jpabook.domain.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        return all;
+    }
+
+}
