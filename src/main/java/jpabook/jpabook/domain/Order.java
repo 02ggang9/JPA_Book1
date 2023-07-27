d290a0f39ec5a945498c1b4756f817dad1ade634
02ggang9 <02ggang9@gmail.com>
2023-07-27 23:20:32 +0900

    JPA-2 Chapter-4: 엔티티를 DTO로 변환 + 페이징과 한계 돌파

--- src/main/java/jpabook/jpabook/domain/Order.java
+++ src/main/java/jpabook/jpabook/domain/Order.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.aspectj.weaver.ast.Or;
+import org.hibernate.annotations.BatchSize;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -27,6 +28,7 @@ public class Order {
     @JoinColumn(name = "member_id")
     private Member member;
 
+    @BatchSize(size = 100)
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
