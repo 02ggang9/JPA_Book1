8f5ca607dbd2f535c472c3c2e2c7baa6f5a30350
02ggang9 <02ggang9@gmail.com>
2023-07-27 14:36:42 +0900

    JPA-2 Chapter-3: V2: 엔티티를 DTO로 변환 - 페치 조인 최적화

--- src/main/java/jpabook/jpabook/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpabook/api/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package jpabook.jpabook.api;
 
+import jpabook.jpabook.domain.Address;
 import jpabook.jpabook.domain.Order;
+import jpabook.jpabook.domain.OrderStatus;
 import jpabook.jpabook.domain.repository.OrderRepository;
 import jpabook.jpabook.domain.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,6 +24,31 @@ public class OrderSimpleApiController {
     public List<Order> ordersV1() {
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
         return all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        return orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDateTime();
+            orderStatus = order.getStatus();
+            address = order.getMember().getAddress();
+        }
     }
 
 }
